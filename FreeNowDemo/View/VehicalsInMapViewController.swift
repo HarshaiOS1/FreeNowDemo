78cded75fe181d0d91b1df712e8023c2fd99d2b0
Harsha k <kharsha21@yahoo.in>
2020-11-10 01:02:47 +0530

    Reduced ios target and cleaned mapVC

--- FreeNowDemo/View/VehicalsInMapViewController.swift
+++ FreeNowDemo/View/VehicalsInMapViewController.swift
@@ -24,66 +24,42 @@ class VehicalsInMapViewController: UIViewController, CLLocationManagerDelegate {
     func updateUI() {
         locationManager = CLLocationManager()
         locationManager.delegate = self
-        
+
         locationManager.requestAlwaysAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startMonitoringSignificantLocationChanges()
         locationManager.requestLocation()
-        
-        
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
-        
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
         let location = CLLocation(latitude:53.5511, longitude: 9.9937) // default pin at hambueg center location
         
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.0005, longitudeDelta: 0.0005))
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
         
         mapView.setRegion(region, animated: true)
         newPin.coordinate = location.coordinate
         mapView.addAnnotation(newPin)
-        
-        
-        
-//        let halfLatDelta = region.span.latitudeDelta / 2
-//        let halfLngDelta = region.span.longitudeDelta / 2
-//
-//        let nePoint:CGPoint = CGPointMake(self.mapView.bounds.origin.x + mapView.bounds.size.width, mapView.bounds.origin.y)
-//        let swPoint:CGPoint = CGPointMake((self.mapView.bounds.origin.x), (mapView.bounds.origin.y + mapView.bounds.size.height))
-//
-//        //Then transform those point into lat,lng values
-//
-//
-//        let topLeftCoord = CLLocationCoordinate2D(
-//            latitude: center.latitude + halfLatDelta,
-//            longitude: center.longitude - halfLngDelta
-//        )
-//        let bottomRightCoord = CLLocationCoordinate2D(
-//            latitude: center.latitude - halfLatDelta,
-//            longitude: center.longitude + halfLngDelta
-//        )
-//
-//
-//        let NECoordinate = CLLocationCoordinate2D(latitude: topLeftCoord.latitude, longitude: topLeftCoord.longitude)
-//        let SWCoordinate = CLLocationCoordinate2D(latitude: bottomRightCoord.latitude, longitude: bottomRightCoord.longitude)
-
+    
         let mRect: MKMapRect = self.mapView.visibleMapRect;
-        let neMapPoint: MKMapPoint = MKMapPoint(x: mRect.maxX, y: mRect.origin.y);
-        let swMapPoint: MKMapPoint = MKMapPoint(x: mRect.origin.x, y: mRect.maxY);
-//        let olaMapPoint: MKMapPoint = MKMapPoint(x: mRect.maxX, y: mRect.maxY);
+        let neMapPoint: MKMapPoint = MKMapPoint(x: mRect.maxX, y: mRect.maxY);
+        let swMapPoint: MKMapPoint = MKMapPoint(x: mRect.origin.x, y: mRect.origin.y);
+        
+        let nwMapPoint: MKMapPoint = MKMapPoint(x: mRect.origin.x, y: mRect.maxY);
+        let seMapPoint: MKMapPoint = MKMapPoint(x: mRect.maxX, y: mRect.origin.y);
+        
         let neCoord = neMapPoint.coordinate;
         let swCoord = swMapPoint.coordinate;
-//        let olaCoord = olaMapPoint.coordinate
-        print(neCoord.latitude)
-        print(neCoord.longitude)
         
-        print(swCoord.latitude)
-        print(swCoord.longitude)
+        let nwCoord = nwMapPoint.coordinate;
+        let seCoord = seMapPoint.coordinate;
         
+        
+        print("northWest: "+String(nwCoord.latitude)+" && "+String(nwCoord.longitude));
+        print("southEast: "+String(seCoord.latitude)+" && "+String(seCoord.longitude));
+        
+        print("northEast: "+String(neCoord.latitude)+" && "+String(neCoord.longitude));
+        print("southWest: "+String(swCoord.latitude)+" && "+String(swCoord.longitude));
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
